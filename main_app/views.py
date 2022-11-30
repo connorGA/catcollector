8c905f1fdf0ad21315f28137dd5dac1d5da712d3
connorGA <ronnoccap@gmail.com>
2022-11-29 21:39:32 -0800

    feat: add views and urls to main_app

---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello world...')
+
+
+def about(request):
+    return HttpResponse('About Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+def blog(request):
+    return HttpResponse('Blog Page')
