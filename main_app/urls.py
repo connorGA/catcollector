983de9414324c3743812b3acc68de5b197f73b2a
connorGA <ronnoccap@gmail.com>
2022-11-30 19:41:48 -0800

    feat: complete urls, views, and templates with bootstrap

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog, name='blog',)
+    path('blog/', views.blog, name='blog'),
+    path('cats/', views.cats_index, name='cats_index'),
 ]
